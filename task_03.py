0c277d8bcb8ec11d8ef0ff4cb1805129c21037cd
Idonets <Idonets@morebis.net>
2024-05-08 00:23:41 +0300

    refactor

--- task_03.py
+++ task_03.py
@@ -1,6 +1,7 @@
 '''module provides functions for the task 3'''
 import re
 import sys
+import pathlib as pl
 from collections import Counter
 
 def parse_log_line(line: str) -> dict:
@@ -14,42 +15,45 @@ def parse_log_line(line: str) -> dict:
 def load_logs(file_path: str) -> list:
     '''load log file'''
     try:
-        with open(file=file_path, mode="r", encoding="utf-8") as file:
-            return [parse_log_line(line.strip()) for line in file if line.lstrip()]
+        if pl.Path(file_path).is_file():
+            with open(file_path, "r", encoding="utf-8") as file:
+                return [parse_log_line(line.strip()) for line in file if line.strip()]
+        raise FileNotFoundError
     except FileNotFoundError:
         return "Error. Log file is not found!"
 
-def filter_logs_by_level(logs: list, level: str) -> list:
+def filter_logs_by_level(log_list: list, log_level: str) -> list:
     '''filter log by level'''
-    return [log for log in logs if log['level'].upper() == level.upper()]
+    return [log for log in log_list if log["level"].upper() == log_level.upper()]
 
-def count_logs_by_level(logs: list) -> dict:
+def count_logs_by_level(log_list: list) -> dict:
     '''count log by level'''
-    levels = [log['level'] for log in logs]
+    levels = [log['level'] for log in log_list]
     return dict(Counter(levels))
 
 def display_log_counts(counts: dict):
     '''show count log by level. Use func count_logs_by_level to count'''
     print("Level of Log".center(17) +"|" + "Count".center(10))
     print("-----------------|----------")
-    for level, count in counts.items():
-        print(f"{level:<17}| {count}")
+    for log_level, count in counts.items():
+        print(f"{log_level:<17}|{count}")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please provide the path to the log file.")
         sys.exit(1)
+    try:
+        log_file_path = sys.argv[1]
+        logs = load_logs(log_file_path)
+        log_counts = count_logs_by_level(logs)
 
-log_file_path = sys.argv[1]
-logs = load_logs(log_file_path)
-log_counts = count_logs_by_level(logs)
-
-if len(sys.argv) == 3:
-    level = sys.argv[2]
-    filtered_logs = filter_logs_by_level(logs, level)
-    print(f"Деталі логів для рівня '{level.upper()}':")
-    for log in filtered_logs:
-        print(f"{log['datetime']} - {log['message']}")
-        print()
-
-display_log_counts(log_counts)
+        if len(sys.argv) == 3:
+            level = sys.argv[2]
+            filtered_logs = filter_logs_by_level(logs, level)
+            print(f"Log details for level '{level.upper()}':")
+            for log in filtered_logs:
+                print(f"{log['datetime']} - {log['message']}")
+        else:
+            display_log_counts(log_counts)
+    except Exception as e:
+        print(e)
